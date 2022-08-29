4fce8317a460b2915c6aefa2f741237593c15731
Mikaeil <mikaeileghbal@yahoo.com>
2022-08-29 19:50:30 +0430

    Add overlay menu with links

--- code/index.js
+++ code/index.js
@@ -1,102 +1,5 @@
-window.addEventListener("load", createLightbox);
-
-function createLightbox() {
-  // Lightbox container
-  const lightBox = document.getElementById("lightbox");
-
-  // Parts of the lightbox
-  const title = document.createElement("h1");
-  const counter = document.createElement("div");
-  const next = document.createElement("button");
-  const prev = document.createElement("button");
-  const play = document.createElement("button");
-  const images = document.createElement("div");
-
-  lightBox.appendChild(title);
-  title.id = "title";
-  title.textContent = lightboxTitle;
-
-  lightBox.appendChild(counter);
-  counter.id = "counter";
-  let currentImage = 1;
-  counter.textContent = currentImage + " / " + imageCount;
-
-  lightBox.appendChild(prev);
-  prev.id = "prev";
-  prev.innerHTML = "&#9664";
-  prev.addEventListener("click", showPrevious);
-
-  lightBox.appendChild(next);
-  next.id = "next";
-  next.innerHTML = "&#9654";
-  next.addEventListener("click", showNext);
-
-  lightBox.append(play);
-  play.id = "play";
-  play.innerHTML = "&#9199";
-  let timerId;
-  play.addEventListener("click", function () {
-    if (timerId) {
-      clearInterval(timerId);
-      timerId = undefined;
-    } else {
-      showNext();
-      timerId = setInterval(showNext, 1500);
-    }
-  });
-
-  lightBox.appendChild(images);
-  images.id = "images";
-
-  // Add images to the container
-  for (let i = 0; i < imageCount; i++) {
-    let img = document.createElement("img");
-    img.src = `images/${imageFiles[i]}`;
-    img.alt = imageCaptions[i];
-    img.addEventListener("click", createOverlay);
-    images.appendChild(img);
-  }
-
-  function showNext() {
-    images.appendChild(images.firstElementChild);
-    currentImage < imageCount ? currentImage++ : (currentImage = 1);
-    counter.textContent = currentImage + " / " + imageCount;
-  }
-
-  function showPrevious() {
-    images.insertBefore(images.lastElementChild, images.firstElementChild);
-    currentImage > 1 ? currentImage-- : (currentImage = imageCount);
-    counter.textContent = currentImage + " / " + imageCount;
-  }
-}
-
-function createOverlay() {
-  let overlay = document.createElement("div");
-  overlay.id = "overlay";
-  let figure = document.createElement("figure");
-
-  let overlayImage = this.cloneNode("true");
-  figure.appendChild(overlayImage);
-
-  let overlayCaptio = document.createElement("figcaption");
-  overlayCaptio.textContent = this.alt;
-  figure.appendChild(overlayCaptio);
-
-  overlay.appendChild(figure);
-
-  let close = document.createElement("div");
-  close.id = "overlay-close";
-  close.innerHTML = "&times";
-  close.addEventListener("click", (e) => {
-    document.body.removeChild(overlay);
-  });
-
-  overlay.appendChild(close);
-  document.body.appendChild(overlay);
-}
-
 window.addEventListener("scroll", function (e) {
-  // if (this.window.scrollY > 300)
+  // if (this.window.scrollY > 360)
   //   this.document.querySelector(".hero-wrap").style.display = "none";
   // else this.document.querySelector(".hero-wrap").style.display = "block";
   animateHero();
@@ -130,6 +33,13 @@ function animateHero() {
   }px, 0) scale(${1 - window.scrollY / 1000})`;
 
   heroImage.style.opacity = 1 - window.scrollY / 350;
+
+  const menuButton = document.querySelector(".nav-button-overlay");
+  if (window.scrollY > 120) {
+    menuButton.classList.add("show");
+  } else {
+    menuButton.classList.remove("show");
+  }
 }
 
 let mouseTail = document.querySelector(".mouse-tail");
